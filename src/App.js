fed2cdfaddca82e28a58ad366b3a7e382cb70ecf
Chae-G-0 <corkdud@naver.com>
2022-11-26 16:22:58 +0900

    221126

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Button from "./Button";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeword(e.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("Call the API...");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 6) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>hello world!</h1>
-      <Button text={"Continue"}/>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      {/* <Button text={"Continue"}/> */}
     </div>
   );
 }
